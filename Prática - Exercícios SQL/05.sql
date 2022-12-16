a0afd82f40e00343f3e2310e60580f114b24ac2b
Ruytter <ruytter.benjamin@gmail.com>
2022-12-16 17:18:38 -0300

    nova tentativa sem sucesso

--- Prática - Exercícios SQL/05.sql
+++ Prática - Exercícios SQL/05.sql
@@ -1,8 +1,20 @@
-SELECT  schools.name AS school, courses.name AS course, COUNT("status") AS studentsCount
+SELECT  schools.name AS school, courses.name AS course, COUNT("courseId") AS studentsCount
 FROM educations
 jOIN schools ON educations."schoolId" = schools.id
 jOIN courses ON educations."courseId" = courses.id
-jOIN users ON educations."userId" = users.id
 WHERE educations.status = 'ongoing' OR educations.status = 'finished'
-GROUP BY schools.id, courses.id, users.id
+GROUP BY schools.id, courses.id
 ORDER BY studentsCount DESC LIMIT 3;
+
+SELECT schools.name AS school, courses.name AS course, COUNT("courseId") AS studentsCount
+FROM educations
+jOIN courses ON educations."courseId" = courses.id
+jOIN schools ON educations."schoolId" = schools.id
+WHERE (educations.status = 'ongoing' 
+OR educations.status = 'finished') 
+AND educations."courseId" = courses.id
+AND educations."schoolId" = schools.id
+GROUP BY schools.id, courses.id
+ORDER BY studentsCount DESC LIMIT 3;
+
+-- Infelizmente não deu
