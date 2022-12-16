454e347f4cdcf4b7aa6d36c1018ba85724df43c8
Ruytter <ruytter.benjamin@gmail.com>
2022-12-16 16:48:07 -0300

    tentativa do bonus com falhas

--- Prática - Exercícios SQL/05.sql
+++ Prática - Exercícios SQL/05.sql
@@ -1,2 +1,8 @@
-SELECT * FROM "clientes";
-DELETE FROM "clientes" WHERE id = 3;
+SELECT  schools.name AS school, courses.name AS course, COUNT("status") AS studentsCount
+FROM educations
+jOIN schools ON educations."schoolId" = schools.id
+jOIN courses ON educations."courseId" = courses.id
+jOIN users ON educations."userId" = users.id
+WHERE educations.status = 'ongoing' OR educations.status = 'finished'
+GROUP BY schools.id, courses.id, users.id
+ORDER BY studentsCount DESC LIMIT 3;
